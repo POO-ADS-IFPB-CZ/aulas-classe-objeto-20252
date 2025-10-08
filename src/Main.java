9caef73cc59a59162cc11add7dfaef09a0fd18e9
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-10-08 07:45:10 -0300

    Adicionando exemplo de static

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
     public static void main(String[] args){
 
-        double ad = 0.1;
-        double bd = 0.2;
-        double somad = ad + bd;
-        System.out.println(somad == 0.3);
-        BigDecimal a = new BigDecimal("0.1");
-        BigDecimal b = new BigDecimal("0.2");
-        BigDecimal soma = a.add(b);
-        System.out.println(new BigDecimal("0.3").equals(soma));
+        Aluno aluno = new Aluno(202012010001l,
+                "João", LocalDate.now(), "ADS", 'M');
+        Aluno aluno2 = new Aluno(202012010001l,
+                "João", LocalDate.now(), "ADS", 'M');
+        Aluno aluno3 = new Aluno(202012010001l,
+                "João", LocalDate.now(), "ADS", 'M');
+
+        System.out.println(Aluno.getContador());
 
     }
 
--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 public class Aluno {
     //Matrícula é constante - não muda
     private final long matricula;
+    //Atributo estático - pertence à classe
+    private static int contador;
     private String nome;
     private LocalDate dataNascimento;
     private String turma;
@@ -12,17 +14,24 @@ public class Aluno {
 
     //Inicializa matrícula e o resto atribui 0 ou null
     public Aluno(long matricula){
+        contador++;
         this.matricula = matricula;
     }
 
     //Construtor com parâmetros
     public Aluno(long matricula, String nome, LocalDate dataNascimento,
                  String turma, char sexo) {
+        contador++;
         this.matricula = matricula;
         this.nome = nome;
         this.dataNascimento = dataNascimento;
         this.turma = turma;
         this.sexo = sexo;
+    }
+
+    //Retorna a quantidade de alunos
+    public static int getContador(){
+        return contador;
     }
 
     //Método get - retorna o atributo
