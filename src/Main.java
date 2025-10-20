12b283dbe7f972275826a3608a735fa00064b76c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-10-20 07:28:25 -0300

    Atualizando o repositório

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        Pessoa pessoa = new Aluno(123l,
-                "João", LocalDate.now(),
-                "ADS", 'M');
+
 
     }
 
